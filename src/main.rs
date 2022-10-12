eea34b696ce2a8e92e5a023696ea66d4b0677d8c
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-10-12 15:38:37 -0700

    added encoding and decoding from radio added heartbeat transmissions added reading commands from api and transmitting over radio

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,104 @@ mod api;
 
 use ArmlabRadio::radio_serial::{Radio, get_radio_ports};
 
-use std::thread;
+use std::slice::from_raw_parts;
+use std::{thread, usize};
 use std::time::{Duration, Instant};
 use std::sync::{Arc, Mutex};
 
+const DATA_STREAM_SIZE: usize = 30;
+
+struct rocket_data {
+    time: u32,
+    altitude: f32,
+    orx: f32,
+    ory: f32,
+    orz: f32,
+    lat: f32,
+    long: f32,
+    fix: u8,
+    quality: u8,
+    cont1: bool,
+    cont2: bool,
+}
+
+fn encode_stream(data: rocket_data) -> Result<[u8; DATA_STREAM_SIZE], String> {
+    let mut buf: Vec<u8> = vec![];
+
+    buf.extend_from_slice(&data.time.to_le_bytes());
+    buf.extend_from_slice(&data.altitude.to_le_bytes());
+    buf.extend_from_slice(&data.orx.to_le_bytes());
+    buf.extend_from_slice(&data.ory.to_le_bytes());
+    buf.extend_from_slice(&data.orz.to_le_bytes());
+    buf.extend_from_slice(&data.lat.to_le_bytes());
+    buf.extend_from_slice(&data.long.to_le_bytes());
+
+
+    let mut fix_qual: u8 = data.quality << 4;
+    fix_qual += data.fix & 0b00001111;
+    buf.push(fix_qual);
+
+    let mut conts: u8 = 0;
+    if data.cont1 {
+        conts += 1;
+    }
+    if data.cont2 {
+        conts += 2;
+    }
+
+    buf.push(conts);
+
+    match buf.as_slice().try_into() {
+        Ok(n) => Ok(n),
+        Err(_) => Err("Error converting vec to slice".to_string()),
+    }
+}
+
+fn decode_stream(buf: [u8; DATA_STREAM_SIZE]) -> Result<rocket_data, String> {
+    let time: u32 = u32::from_le_bytes(match buf[0..4].try_into(){
+        Ok(n) => n,
+        Err(_) => {return Err("error converting time to u32".to_string())},
+    });
+
+    let altitude: f32 = f32::from_le_bytes(match buf[4..8].try_into() {
+        Ok(n) => n,
+        Err(_) => {return Err("error converting altitude to f32".to_string())},
+    });
+    let orx: f32 = f32::from_le_bytes(match buf[8..12].try_into() {
+        Ok(n) => n,
+        Err(_) => {return Err("error converting orx to f32".to_string())},
+    });
+    let ory: f32 = f32::from_le_bytes(match buf[12..16].try_into() {
+        Ok(n) => n,
+        Err(_) => {return Err("error converting ory to f32".to_string())},
+    });
+    let orz: f32 = f32::from_le_bytes(match buf[16..20].try_into() {
+        Ok(n) => n,
+        Err(_) => {return Err("error converting orz to f32".to_string())},
+    });
+    let lat: f32 = f32::from_le_bytes(match buf[20..24].try_into() {
+        Ok(n) => n,
+        Err(_) => {return Err("error converting lat to f32".to_string())},
+    });
+    let long: f32 = f32::from_le_bytes(match buf[24..28].try_into() {
+        Ok(n) => n,
+        Err(_) => {return Err("error converting long to f32".to_string())},
+    });
+
+    // 28: 0000 0000
+    //     qual fix
+    let fix: u8 = buf[28] | 0b00001111;// first 4 (least significant) bits of 29
+    let quality: u8 = buf[28] >> 4;// last 4 bits of 29
+
+    // 00000   0   0
+    //         2   1
+    let cont1: bool = buf[29] & 1 == 1; // first (lsb) of 30
+    let cont2: bool = buf[29] & 2 == 1; // second (lsb) of 30
+    
+
+
+    Ok(rocket_data {time, altitude, orx, ory, orz, lat, long, fix, quality, cont1, cont2})
+}
 
 fn get_user_port() -> String{
     let radios = get_radio_ports().expect("error getting devices");
@@ -60,12 +154,22 @@ fn get_user_port() -> String{
 
 }
 
+
+
+
 fn radio(arc_data: api::TData) {
-    let mut radio = Radio::new(&get_user_port()).expect("Error Creating Radio");
+    let port = get_user_port();
+
+    println!("found radio on port {}", port);
+
+    let mut radio = Radio::new(&port).expect("Error Creating Radio");
     radio.set_power(14f32).expect("error setting power");
 
+    let abs_start = Instant::now();
+    let mut altitude: f32 = 0f32;
     let mut start_time = Instant::now();
     let mut good_connection = true;
+    let mut iter: usize = 0;
 
     loop {
         let mut data = arc_data.lock().unwrap();
@@ -78,26 +182,89 @@ fn radio(arc_data: api::TData) {
         // handle commands
         for command in data.cmds.iter() {
             let (cmd, arg) = command;
+
+            println!("got cmd {}, with args {}", cmd, arg);
+
+            let mut buf: [u8; 5] = [0u8; 5];
             match cmd.as_str() {
+                "test" => {
+                    buf[0] = 2;
+                    
+                    let f = arg.to_le_bytes();
+                    buf[1] = f[0];
+                    buf[2] = f[1];
+                    buf[3] = f[2];
+                    buf[4] = f[3];
+
+                }
                 _ => {
                     println!("unknown command");
                 }
             }
+            
+            radio.transmit(&buf).expect("error transmitting");
         }
         data.cmds.clear();
 
-        if good_connection && start_time.elapsed() >= Duration::from_millis(2000) {
+        // downlink
+        {
+            // get data stream
+            let buf = match radio.get_packet() {
+                Ok(n) => {
+                    if n.len() == 0 {
+                        return;
+                    }
+                    n
+                },
+                Err(_) => {return;}
+            };
+
+            let buf: [u8; DATA_STREAM_SIZE] = match buf.try_into() {
+                Ok(n) => n,
+                Err(n) => {
+                    println!("Error | expected length {} got {} ", DATA_STREAM_SIZE, n.len());
+                    return;
+                }
+            };
+
+            let rec_data = match decode_stream(buf) {
+                Ok(n) => n,
+                Err(n) => {
+                    println!("Error decoding stream | {}", n);
+                    return;
+                }
+            };
+
+            let time: f32 = rec_data.time as f32 / 1000f32;
+
+            data.altitude.push((time, rec_data.altitude)); 
+            data.orx.push((time, rec_data.orx));
+            data.ory.push((time, rec_data.ory));
+            data.orz.push((time, rec_data.orz));
+            data.lat.push((time, rec_data.lat));
+            data.long.push((time, rec_data.long));
+            data.fix.push((time, rec_data.fix as f32));
+            data.quality.push((time, rec_data.quality as f32));
+            data.cont_droug.push((time, if rec_data.cont1 {1f32} else {0f32}));
+            data.cont_main.push((time, if rec_data.cont2 {1f32} else {0f32}));
+
+        }
+
+        drop(data);
+
+        // if we are unable to parse a data stream we continue; this skips the heartbeat section alltogether
+        if start_time.elapsed() >= Duration::from_millis(2000) {
+            iter += 1;
             // transmit heartbeat
+            println!("sending heartbeat {}", iter);
             
             start_time = Instant::now();
+            radio.transmit(&[1, 1, 1, 1, 1]).expect("transmit error");
         }
 
 
-        //handle data downlink
-        //radio.transmit()
-        //radio.get_packet()
-
-        drop(data);
+        
+        // give api a chance to aquire mutex lock
         thread::sleep(Duration::from_millis(50));
     } 
 
